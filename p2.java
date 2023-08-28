ba1819d3825ec7c6f2b8fdd49c4f90328584494a
Rishav <rishav2freelance@gmail.com>
2023-08-29 01:00:59 +0530

    Asking rollNo

---
+++ p2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class p2 {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+         
+        int rollNo = input.nextInt();
+       System.out.println("Enter Roll no. - "+ rollNo);
+    }
+}
