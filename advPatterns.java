eec48990c3bd3cfe69101b6174da185c89125cd7
Rishav <rishav2freelance@gmail.com>
2023-09-01 17:10:36 +0530

    Adding number pattern in java

--- advPatterns.java
+++ advPatterns.java
@@ -70,22 +70,22 @@ public class advPatterns {
 
 
 
-// for(int i=1; i<=n; i++){
-//     int spaces=n-i;
-//     for(int j=1; j<=spaces;j++){
-//         System.out.print(" ");
-//     }
-//     for(int j=i; j>=1;j--){
-//         System.out.print(j);
+for(int i=1; i<=n; i++){
+    int spaces=n-i;
+    for(int j=1; j<=spaces;j++){
+        System.out.print(" ");
+    }
+    for(int j=i; j>=1;j--){
+        System.out.print(j);
       
-//     }
-//     for(int j=2; j<=i; j++){
-//         System.out.print(j);
+    }
+    for(int j=2; j<=i; j++){
+        System.out.print(j);
        
-//     }
-//     System.out.println();
+    }
+    System.out.println();
 
-// }
+}
 
 
 
